7ab954c50dd2c21bbe6e47eefc3ec0d0ea1dfa5c
Ayachi Gin <ayachigin@gmail.com>
2016-11-12 17:10:34 +0900

    NAND OR NOT

--- src/Main.hs
+++ src/Main.hs
@@ -5,18 +5,28 @@ type Threshold = Double
 
 data Perceptron = Perceptron { w1 :: Double
                              , w2 :: Double
-                             , theta :: Double
+                             , bias :: Double
                              } deriving (Show, Read, Ord, Eq)
 
 main :: IO ()
 main = do
   putStrLn "hello world"
 
-andP :: Num a => Double -> Double -> a
-andP = perceptron (Perceptron 0.5 0.5 0.8)
+andP :: Double -> Double -> Double
+andP = perceptron (Perceptron 0.5 0.5 (-0.8))
 
-perceptron :: Num a => Perceptron -> Double -> Double -> a
-perceptron (Perceptron w1 w2 t) x y = if w1 * x + w2 * y <= t then
-                                        0
-                                      else
-                                        1
+nandP :: Double -> Double -> Double
+nandP = perceptron (Perceptron (-0.5) (-0.5) 0.8)
+
+orP = perceptron (Perceptron 0.1 0.1 0)
+
+notP = perceptron (Perceptron 1 (-1) 0) 1
+
+perceptron :: Perceptron -> Double -> Double -> Double
+perceptron (Perceptron w1 w2 b) x y =
+  if val <= 0 then
+    0
+  else
+    1
+  where
+    val = (b+) . sum $ zipWith (*) [x, y] [w1, w2]
