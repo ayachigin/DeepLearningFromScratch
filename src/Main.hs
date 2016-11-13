cb6ba2d09e8c5454ef0db374a4a48e4333f7ba0d
Ayachi Gin <ayachigin@gmail.com>
2016-11-13 19:15:39 +0900

    My implementation

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,60 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Main where
 
-type Weight = Double
-type Threshold = Double
+--import Numeric.LinearAlgebra
 
-data Perceptron = Perceptron { w1 :: Double
-                             , w2 :: Double
-                             , bias :: Double
-                             } deriving (Show, Read, Ord, Eq)
+import Data.List (transpose)
+
+data Matrix = V0 Double
+            | V1 [Double]
+            | V2 [[Double]]
+            deriving (Show, Read, Ord, Eq)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  let x = V1 [1.0, 0.5]
+      w1 = V2 [ [0.1, 0.3, 0.5]
+              , [0.2, 0.4, 0.6]]
+      b1 = V1 [0.1, 0.2, 0.3]
+      a1 = add (dot x w1) b1
+      z1 = apply sigmonoid a1
+      w2 = V2 [[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]
+      b2 = V1 [0.1, 0.2]
+      a2 = add (dot z1 w2) b2
+      z2 = apply sigmonoid a2
+      w3 = V2 [[0.1, 0.3], [0.2, 0.4]]
+      b3 = V1 [0.1, 0.2]
+      a3 = add (dot z2 w3) b3
+      z3 = apply id a3
+  print z3
 
-andP :: Double -> Double -> Double
-andP = perceptron (Perceptron 0.5 0.5 (-0.8))
+sigmonoid x = 1 / (1 + exp (-x))
 
-nandP :: Double -> Double -> Double
-nandP = perceptron (Perceptron (-0.5) (-0.5) 0.8)
+softmax x = let c = maximum x
+                expA = map (\a -> exp $ a - c) x
+                sumExpA = sum expA
+            in map (/sumExpA) expA
 
-orP = perceptron (Perceptron 0.1 0.1 0)
 
-notP = perceptron (Perceptron 1 (-1) 0) 1
-
-xorP x y = andP (nandP x y) (orP x y)
-
-perceptron :: Perceptron -> Double -> Double -> Double
-perceptron (Perceptron w1 w2 b) x y =
-  if val <= 0 then
-    0
-  else
-    1
+dot :: Matrix -> Matrix -> Matrix
+dot x y = case (x, y) of
+            ((V1 xs), (V1 ys)) -> V0 $ dot' xs ys
+            ((V2 xs), (V1 ys)) -> V1 $ map (\ls -> dot' ls ys) xs
+            ((V1 xs), (V2 ys)) -> V1 $ dot'' xs ys
+            ((V2 xs), (V2 ys)) -> V2 $ map (\x -> dot'' x ys) xs
   where
-    val = (b+) . sum $ zipWith (*) [x, y] [w1, w2]
+    dot' = (sum .) . zipWith (*)
+    dot'' a = map (dot' a) . transpose
+
+apply :: (Double -> Double) -> Matrix -> Matrix
+apply f (V0 a) = V0 $ f a
+apply f (V1 ls) = V1 $ map f ls
+apply f (V2 ls) = V2 $ map (map f) ls
+
+add :: Matrix -> Matrix -> Matrix
+add (V0 x) (V0 y) = V0 $ x + y
+add (V0 x) (V1 y) = V1 $ map (+x) y
+add (V1 x) (V0 y) = V1 $ map (+y) x
+add (V0 x) (V2 y) = V2 $ map (map (+x)) y
+add (V2 x) (V0 y) = V2 $ map (map (+y)) x
+add (V1 x) (V1 y) = V1 $ zipWith (+) x y
