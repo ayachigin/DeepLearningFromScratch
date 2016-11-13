dba74aec3664271a70ebad4e3babf9926ec071d8
Ayachi Gin <ayachigin@gmail.com>
2016-11-13 21:09:29 +0900

    Repa version

--- src/Main.hs
+++ src/Main.hs
@@ -1,60 +1,47 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Main where
 
---import Numeric.LinearAlgebra
+import Prelude hiding (map, zipWith)
+import Data.Array.Repa
+import Data.Array.Repa.Algorithms.Matrix
 
-import Data.List (transpose)
+ain = do
+  let w1 = (fromListUnboxed (ix2 1 2) [1.0, 0.5]::Array U DIM2 Double)
+  v <- mmultP w1 x2
+  print $ toList (w1:: Array U DIM2 Double)
+  print $ toList v --(v :: Array U DIM2 Double)
+  return ()
+      --where
 
-data Matrix = V0 Double
-            | V1 [Double]
-            | V2 [[Double]]
-            deriving (Show, Read, Ord, Eq)
+x2 :: Array U DIM2 Double
+x2 = fromListUnboxed (ix2 2 3) [0.1, 0.3, 0.5, 0.2, 0.4, 0.6]
+xs' :: Array U DIM1 Int
+xs' = fromListUnboxed (ix1 2) [1, 5]
+
+type Matrix = Array U DIM2 Double
 
 main :: IO ()
 main = do
-  let x = V1 [1.0, 0.5]
-      w1 = V2 [ [0.1, 0.3, 0.5]
-              , [0.2, 0.4, 0.6]]
-      b1 = V1 [0.1, 0.2, 0.3]
-      a1 = add (dot x w1) b1
-      z1 = apply sigmonoid a1
-      w2 = V2 [[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]
-      b2 = V1 [0.1, 0.2]
-      a2 = add (dot z1 w2) b2
-      z2 = apply sigmonoid a2
-      w3 = V2 [[0.1, 0.3], [0.2, 0.4]]
-      b3 = V1 [0.1, 0.2]
-      a3 = add (dot z2 w3) b3
-      z3 = apply id a3
-  print z3
+  let x  = fromListUnboxed (ix2 1 2) [1.0, 0.5]
+      w1 = fromListUnboxed (ix2 2 3) [0.1, 0.3, 0.5, 0.2, 0.4, 0.6]
+      b1 = fromListUnboxed (ix2 1 3) [0.1, 0.2, 0.3]
+      a1 = zipWith (+) b1 $ mmultS x w1
+      z1 = computeS $ map sigmonoid a1
+      w2 = fromListUnboxed (ix2 3 2) [0.1, 0.4, 0.2, 0.5, 0.3, 0.6]
+      b2 = fromListUnboxed (ix2 1 2) [0.1, 0.2]
+      a2 = zipWith (+) b2 $ mmultS z1 w2
+      z2 = computeS $ map sigmonoid a2
+      w3 = fromListUnboxed (ix2 2 2) [0.1, 0.3, 0.2, 0.4]
+      b3 = fromListUnboxed (ix2 1 2) [0.1, 0.2]
+      a3 = zipWith (+) b3 $ mmultS z2 w3
+      z3 = a3
+  print $ toList z3
 
+sigmonoid :: Double -> Double
 sigmonoid x = 1 / (1 + exp (-x))
 
+softmax :: [Double] -> [Double]
 softmax x = let c = maximum x
-                expA = map (\a -> exp $ a - c) x
+                expA = fmap (\a -> exp $ a - c) x
                 sumExpA = sum expA
-            in map (/sumExpA) expA
-
-
-dot :: Matrix -> Matrix -> Matrix
-dot x y = case (x, y) of
-            ((V1 xs), (V1 ys)) -> V0 $ dot' xs ys
-            ((V2 xs), (V1 ys)) -> V1 $ map (\ls -> dot' ls ys) xs
-            ((V1 xs), (V2 ys)) -> V1 $ dot'' xs ys
-            ((V2 xs), (V2 ys)) -> V2 $ map (\x -> dot'' x ys) xs
-  where
-    dot' = (sum .) . zipWith (*)
-    dot'' a = map (dot' a) . transpose
-
-apply :: (Double -> Double) -> Matrix -> Matrix
-apply f (V0 a) = V0 $ f a
-apply f (V1 ls) = V1 $ map f ls
-apply f (V2 ls) = V2 $ map (map f) ls
-
-add :: Matrix -> Matrix -> Matrix
-add (V0 x) (V0 y) = V0 $ x + y
-add (V0 x) (V1 y) = V1 $ map (+x) y
-add (V1 x) (V0 y) = V1 $ map (+y) x
-add (V0 x) (V2 y) = V2 $ map (map (+x)) y
-add (V2 x) (V0 y) = V2 $ map (map (+y)) x
-add (V1 x) (V1 y) = V1 $ zipWith (+) x y
+            in fmap (/sumExpA) expA
