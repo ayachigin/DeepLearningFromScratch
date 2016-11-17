2b4846934d115fda2070d1cffd8b0391e0e49018
Ayachi Gin <ayachigin@gmail.com>
2016-11-17 09:37:35 +0900

    Add numericalGradient function

--- src/Main3.6.hs
+++ src/Main3.6.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude hiding (map, zipWith)
 import Data.Array.Repa hiding ((++))
+import qualified Data.Array.Repa as R
 import Data.Array.Repa.Algorithms.Matrix (mmultS)
 import Data.Array.Repa.Repr.Vector (V, computeVectorP, fromListVector)
 import System.Random (randomRs, mkStdGen)
@@ -51,6 +52,15 @@ main = do
     b2 = matrix (ix2 1 100) 100 ((-10), 100) 20
     x3 = matrix (ix2 100 10) (100 * 10) ((-10), 100) 5
     b3 = matrix (ix2 1 10) 10 ((-10), 10) 100
+
+numericalGradient :: ([Double] -> Double) -> [Double] -> [Double]
+numericalGradient f (l:ls) = g f [] l ls
+  where
+    g f left val [] = [diff f left val []]
+    g f left val rrs@(r:rs) = diff f left val rrs:
+                              g f (left++[val]) r rs
+    diff f l x r = (f (l ++ [x+h] ++ r) - f (l ++ [x-h] ++ r)) / 2*h
+    h = 0.1^3
 
 {- | loass function meanSquaredError
 >>> let y = fromListUnboxed (ix2 1 10) [0.1, 0.05, 0.1, 0, 0.05, 0.1, 0, 0.6, 0, 0]
