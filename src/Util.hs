8130802d048c986ae249da175e4afbf7e4f233ff
Ayachi Gin <ayachigin@gmail.com>
2016-11-17 09:38:25 +0900

    Add Util module

---
+++ src/Util.hs
@@ -0,0 +1,18 @@
+module Util where
+
+import Data.Array.Repa hiding ((++))
+import qualified Data.Array.Repa as R
+
+modifyA :: (Source r e, Shape sh) =>
+          Array r sh e -> Int -> e -> Array D sh e
+modifyA arr i v = R.traverse arr id f
+  where
+    originalShape = extent arr
+    f g sh
+      | toIndex originalShape sh == i = v
+      | otherwise                     = g sh
+
+modifyL :: [a] -> Int -> a -> [a]
+modifyL ls i e = l1++(e:l2)
+  where
+    (l1, (_:l2)) = splitAt i ls
